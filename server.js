8f10c9f6549c786ec1bc01df6823fd47eb18aedc
Yohana <yohana.contrerasg@gmail.com>
2020-11-05 17:26:07 -0400

    cambio en rutas

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const PORT = process.env.PORT || 5000;
 app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
+// Route
+app.get('/', (req, res) => {
+  res.send('Welcome to my API!');
+});
+
 const apiRoutes = require("./routes/apiRoutes");
 app.use('/api', apiRoutes);
 
