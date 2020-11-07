1164409c84badacaeb01573b1ca3204643a14828
Yohana <yohana.contrerasg@gmail.com>
2020-11-06 22:50:34 -0400

    version 4

--- routes/user-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-var UserController = require("../controllers/user-controller");
-
-const router = express.Router();
-
-router.post('/user/register', UserController.save);
-
-module.exports = router;
