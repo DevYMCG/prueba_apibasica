f4d5927e8fe8ede7b3c3f5d2a21091a887c0208c
Yohana <yohana.contrerasg@gmail.com>
2020-11-07 20:04:30 -0400

    version 6

--- controllers/school-controller.js
+++ controllers/school-controller.js
@@ -1,21 +1,63 @@
 const db = require('../models');
+var validator = require('validator');
+const bcrypt = require('bcrypt');
 
 var controller = {
 
 	save: function(req, res){
-		db.School.create({
-		 rut: req.body.rut,
-		 name: req.body.name,
-		 picture: req.body.email,
-		 address: req.body.address,
-		 telephone: req.body.telephone,
-		 url: req.body.url,
-		 color: req.body.color,
-		 status: req.body.status,
-		 date_last_data: req.body.date_last_data,
-		 fee: req.body.fee,
-		 solvency: req.body.solvency
-		}).then(newSchool => res.send(newSchool));
+
+		// Recoger los parametros de la peticion
+		var params = req.body;
+
+		try{
+			var validate_name = !validator.isEmpty(params.name);
+			var validate_email = !validator.isEmpty(params.email);
+			var validate_address = !validator.isEmpty(params.address);
+			var validate_telephone = !validator.isEmpty(params.telephone);
+		}catch(err){
+			return res.status(200).send({
+				message: 'Faltan datos por enviar'
+			});
+		}
+
+		if( validate_name  && validate_email  && validate_address && validate_telephone){
+			// Crear objeto del representante
+			var school = new db.School();
+
+			// Asignar valores al representante
+			school.rut = params.rut;
+			school.name = params.name;
+			school.picture = params.picture;
+			school.email = params.email;
+			school.address = params.address;
+			school.telephone = params.telephone;
+			school.url = params.url;
+			school.color = "#FFFFFF";
+			school.status = 'A';
+			school.date_last_data = params.date_last_data;
+			school.fee = params.fee;
+			school.solvency = 'A';
+
+			school.save().then(function(newSchool, created){
+				if(!newSchool){
+					return res.status(200).send({
+					message: 'La Institucion no se ha guardado'
+					});
+				}
+
+				// Devolver respuesta
+				return res.status(200).send({
+					status: 'success',
+					school: newSchool
+				});						
+			});	
+
+			// Devolver respuesta
+		}else{
+			return res.status(200).send({
+			message: 'La validacion de los datos son incorrectos, intentalo de nuevo'
+			});
+		}
 	},
 
 	getSchools: function(req, res){
