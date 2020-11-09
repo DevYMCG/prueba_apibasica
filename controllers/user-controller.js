e090daf63fa79fba8a6450265a3d437aa3e8debf
Yohana <yohana.contrerasg@gmail.com>
2020-11-09 17:23:02 -0400

    version 12

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -191,7 +191,7 @@ var controller = {
 				if(user && user.loginname == params.loginname){
 					return res.status(200).send({
 						message: 'El loginname ya existe',
-						db.User.loginname
+						user
 					});
 				}else{
 
