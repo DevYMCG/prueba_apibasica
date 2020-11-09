f492ea07d2c5c0405acd2bed6617d12a578dbfae
Yohana <yohana.contrerasg@gmail.com>
2020-11-08 20:31:14 -0400

    version 10

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -158,7 +158,81 @@ var controller = {
 		});
 	},
 
-		update: function(req, res){
+	update: function(req, res){
+		// Recoger los parametros de la peticion
+		var params = req.body;
+
+		// Validar los datos 
+		try{
+			var validate_loginname = !validator.isEmpty(params.loginname);
+			var validate_surname = !validator.isEmpty(params.surname);
+			var validate_name = !validator.isEmpty(params.name);
+			var validate_password = !validator.isEmpty(params.password);
+			var validate_parent = !validator.isEmpty(params.parentId);
+			var validate_role = !validator.isEmpty(params.roleId);
+			var validate_school = !validator.isEmpty(params.schoolId);
+
+		}catch(err){
+			return res.status(400).send({
+				message: 'Faltan datos por enviar'
+			});
+		}
+
+		if(validate_loginname && validate_surname && validate_name && validate_password){
+			// Crear objeto de usuario
+			var user = new db.User();
+
+			// Asignar valores de usuario
+			user.loginname = params.loginname;
+			user.surname = params.surname;
+			user.name = params.name;
+			user.password = params.password;
+			user.url = params.url;
+			user.ParentId = params.parentId;
+			user.RoleId = params.roleId;
+			user.SchoolId = params.schoolId;
+
+			// Comprobar si el usuario existe
+			db.User.findOne({
+				where: {
+					loginname: user.loginname
+				}
+			}).then(function(issetUser){
+
+				if(issetUser){
+					return res.status(500).send({
+						message: 'Error al comprobar duplicidad del loginname'
+					});
+				}else{
+					// Si no existe, cifrar la contraseña y guardar
+					bcrypt.hash(params.password, 10, function(err, hash){
+					user.password = hash;
+					//guardar usuario 
+
+					user.save().then(function(newUser, created){
+						if(!newUser){
+							return res.status(400).send({
+							message: 'El usuario no se ha guardado'
+							});
+						}
+					// Limpiar el objto
+					user.password = undefined;
+
+					// Devolver respuesta
+					return res.status(201).send({
+						status: 'success',
+						user: newUser
+					});						
+				});
+				
+				});
+				}
+		 })
+		}else{
+			return res.status(400).send({
+			message: 'Validación de los datos del usuario, incorrecta, intentalo de nuevo'
+			});
+		}
 
 	},
 
