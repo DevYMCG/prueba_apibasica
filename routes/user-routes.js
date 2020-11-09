239a8421940f7243972734daf18df5bc5b643de7
Yohana <yohana.contrerasg@gmail.com>
2020-11-09 19:25:36 -0400

    version 202

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -4,11 +4,14 @@ var UserController = require("../controllers/user-controller");
 const router = express.Router();
 var md_auth = require('../middlewares/authenticate'); 
 
+var multipart = require('connect-multiparty');
+var md_upload = multipart({uploadDir: './uploads/users'});
+
 router.post('/user/register', UserController.save);
 router.post('/login', UserController.login);
 router.put('/update', md_auth.authenticate,UserController.update);
 router.get('/users', UserController.getUsers);
 router.get('/user/:id', UserController.getUser);
-router.post('/upload-avatar', UserController.uploadAvatar);
+router.post('/upload-avatar', [md_auth.authenticate, md_upload], UserController.uploadAvatar)
 
 module.exports = router;
